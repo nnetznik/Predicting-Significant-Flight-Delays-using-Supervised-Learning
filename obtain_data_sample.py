f70dde419b85892bac2020e0d6ce57ce2da01802
Nate Netznik <nnetznik@deloitte.com>
2023-01-05 14:25:06 -0500

    Built upon models

--- obtain_data_sample.py
+++ obtain_data_sample.py
@@ -12,7 +12,7 @@ path = "C:/Users/nnetznik/Documents/Capstone/data/"
 files = os.listdir(path)
  
 df_list = [pd.read_csv(path+file) for file in files]
-df = pd.concat(df_list, axis=0).sample(25000, axis=0, random_state=100)
+df = pd.concat(df_list, axis=0).sample(100000, axis=0, random_state=100)
 
 df.to_csv(path+'sample.csv')
 
